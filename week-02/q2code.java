0e7e20cfa609da23b1bc05688bbc44f235d90f04
arjunguptatnp <agupta110@hawk.iit.edu>
2017-01-26 16:05:15 -0600

    Update q2code.java
    References of portions of code used from the internet added

--- week-02/q2code.java
+++ week-02/q2code.java
@@ -1,5 +1,18 @@
-//References: https://lstsal.wordpress.com/2015/08/23/mapreduce-programme-find-highest-temperature-for-each-year-in-ncdc-data-set/
-//References: http://beginnersbook.com/2013/12/java-string-substring-method-example/
+//Reference: https://lstsal.wordpress.com/2015/08/23/mapreduce-programme-find-highest-temperature-for-each-year-in-ncdc-data-set/
+//Reference: http://beginnersbook.com/2013/12/java-string-substring-method-example/
+//Reference: http://www.java2s.com/Code/Java/File-Input-Output/Readsomedatafromagzipfile.htm
+//Reference: https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
+//Reference: http://www.java2s.com/Code/JavaAPI/java.sql/PreparedStatementsetStringintparameterIndexStringx.htm
+
+/*
+Concept used: I have first used a tutorial from the internet(link added) to connect to the database.
+After that used the concept of FileInput Stream and GZIP input stream to read the file.
+Then using the schema provided in the book, I used substringfunction to parse the data
+Then I inserted the data into the database.
+I finally used 
+
+
+*/
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -17,60 +30,53 @@ import java.util.Calendar;
 public class q2code {
 	public static void main(String[] args) throws Exception {
 		 String myDriver = "org.gjt.mm.mysql.Driver";
-	     String myUrl = "jdbc:mysql://localhost/arjweek2";
+	     String myUrl = "jdbc:mysql://localhost/arjweek2"; //arjweek2 is name of the schema
 	     Class.forName(myDriver);
-	     Connection conn = DriverManager.getConnection(myUrl, "root", "password");
+	     Connection conn = DriverManager.getConnection(myUrl, "root", "password"); //this part has been taken from the internet
+		//Reference of above part is: https://www.tutorialspoint.com/jdbc/jdbc-sample-code.htm
 	    
-		FileInputStream fis2 = new FileInputStream("/home/ubuntu/all/1990.gz");
-		GZIPInputStream gis = new GZIPInputStream(fis2);
-		//FileOutputStream fos2 = new FileOutputStream("/home/ubuntu/all/1990.csv");
-		Reader decoder = new InputStreamReader(gis);
+		FileInputStream f1 = new FileInputStream("/home/ubuntu/all/1990.gz"); //I put the .gz file in 'all' folder
+		GZIPInputStream g1 = new GZIPInputStream(f1);
+		Reader decoder = new InputStreamReader(g1);
 		BufferedReader br = new BufferedReader(decoder);
-		String line;
-		while((line  = br.readLine()) != null){
-		//System.out.println(line);
-                String ul1 = line.substring(0,3);
-                String usafwsi = line.substring(4,9);
-                String wban= line.substring(10,14);
-		String Year = line.substring(15,19);
-                String DateMonth = line.substring(19,22);
-		String hour = line.substring(23,26);
-                String ul2 = line.substring(26,27); //should be 27 only but it is useless so no issues
-		String latitude = line.substring(28,33);
-                String longitude = line.substring(34,40);
-                String ul3 = line.substring(41,45); 
-		String elevation = line.substring(46,50);
-                String ul4 = line.substring(51,55);
-                String ul5 = line.substring(56,59);
-		String wind_direction = line.substring(60,62);
-                String qc1 = line.substring(62,63); //should be 63 only but we are not displaying it so no issues
-     		String ul6 = line.substring(63,64); //should be 64 only but it is useless so no issues	
-                String ul7 = line.substring(65,68);
-                String ul8 = line.substring(68,69); //should be 69 only but it is useless so no issues
-                String sky_cieling= line.substring(70,74);
-                String qc2 = line.substring(74,75); //should be 75 only but we are not displaying it so no issues  
-                String ul9 = line.substring(75,76); //should be 76 only but it is useless so no issues
-                String ul10 = line.substring(76,77); //should be 77 only but it is useless so no issues
-		String visibility= line.substring(78,83); 
-                String qc3 = line.substring(83,84); //should be 84 only but we are not displaying it so no issues
-                String ul11 = line.substring(84,85); //should be 85 only but it is useless so no issues
-                String ul12 = line.substring(85,86); //should be 86 only but it is useless so no issues
-                String Temp = line.substring(87,91);
-		String qc4 = line.substring(91,92); //should be 92 only but we are not displaying it so no issues
-		String dew_point = line.substring(93,97);   
-                String qc5 = line.substring(97,98); //should be 98 only but we are not displaying it so no issues
-		String atm_pressure= line.substring(99,103);   
-                String qc6 = line.substring(103,104); //should be 104 only but we are not displaying it so no issues                        
-                               
-                               
-        
-
-
-
-		
-		
-	//	System.out.println(qc1);
-		String query = "insert into  arjweek2(ul1,usafwsi,wban,Year,DateMonth,hour,ul2,latitude,longitude,ul3,elevation,ul4,ul5,wind_direction,qc1,ul6,ul7,ul8,sky_cieling,qc2,ul9,ul10,visibility,qc3,ul11,ul12,Temp,qc4,dew_point,qc5,atm_pressure,qc6) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+		//Reference of above part is: http://www.java2s.com/Code/Java/File-Input-Output/Readsomedatafromagzipfile.htm
+		String l1;
+		while((l1  = br.readLine()) != null)
+		{
+                String ul1 = l1.substring(0,3);
+                String usafwsi = l1.substring(4,9);
+                String wban= l1.substring(10,14);
+		String Year = l1.substring(15,19);
+                String DateMonth = l1.substring(19,22);
+		String hour = l1.substring(23,26);
+                String ul2 = l1.substring(26,27); //should be 27 only but it is useless so no issues
+		String latitude = l1.substring(28,33);
+                String longitude = l1.substring(34,40);
+                String ul3 = l1.substring(41,45); 
+		String elevation = l1.substring(46,50);
+                String ul4 = l1.substring(51,55);
+                String ul5 = l1.substring(56,59);
+		String wind_direction = l1.substring(60,62);
+                String qc1 = l1.substring(62,63); //should be 63 only but we are not displaying it so no issues
+     		String ul6 = l1.substring(63,64); //should be 64 only but it is useless so no issues	
+                String ul7 = l1.substring(65,68);
+                String ul8 = l1.substring(68,69); //should be 69 only but it is useless so no issues
+                String sky_cieling= l1.substring(70,74);
+                String qc2 = l1.substring(74,75); //should be 75 only but we are not displaying it so no issues  
+                String ul9 = l1.substring(75,76); //should be 76 only but it is useless so no issues
+                String ul10 = l1.substring(76,77); //should be 77 only but it is useless so no issues
+		String visibility= l1.substring(78,83); 
+                String qc3 = l1.substring(83,84); //should be 84 only but we are not displaying it so no issues
+                String ul11 = l1.substring(84,85); //should be 85 only but it is useless so no issues
+                String ul12 = l1.substring(85,86); //should be 86 only but it is useless so no issues
+                String Temp = l1.substring(87,91);
+		String qc4 = l1.substring(91,92); //should be 92 only but we are not displaying it so no issues
+		String dew_point = l1.substring(93,97);   
+                String qc5 = l1.substring(97,98); //should be 98 only but we are not displaying it so no issues
+		String atm_pressure= l1.substring(99,103);   
+                String qc6 = l1.substring(103,104); //should be 104 only but we are not displaying it so no issues                        
+                           
+		String query = "insert into  arjweek2(ul1,usafwsi,wban,Year,DateMonth,hour,ul2,latitude,longitude,ul3,elevation,ul4,ul5,wind_direction,qc1,ul6,ul7,ul8,sky_cieling,qc2,ul9,ul10,visibility,qc3,ul11,ul12,Temp,qc4,dew_point,qc5,atm_pressure,qc6);";
 		PreparedStatement preparedStmt = conn.prepareStatement(query);
              preparedStmt.setString (1, ul1);
 	     preparedStmt.setString (2,usafwsi);
@@ -104,6 +110,7 @@ public class q2code {
 	     preparedStmt.setString (30, qc5);
 	     preparedStmt.setString (31, atm_pressure);
              preparedStmt.setString (32, qc6);
+	//Reference of above part is: http://www.java2s.com/Code/JavaAPI/java.sql/PreparedStatementsetStringintparameterIndexStringx.htm
 			
 	     //32 parameters instead of 31 is being reported as error
 	     preparedStmt.executeUpdate();
