730be00d961445d45615e5163090d4ea06e9210b
arjunguptatnp <agupta110@hawk.iit.edu>
2017-04-29 22:50:06 -0500

    Add files via upload
    Month.sql file added.

---
+++ week-13/item-two/month.sql
@@ -0,0 +1,1 @@
+select p.month1,p.year1,max(p.url),max(p.count) as max from ((select month(date1) as month1, year(date1) as year1, count(cs_uri_stem) as count,cs_uri_stem as url from smalllog where sc_status=200 and cs_uri_stem NOT LIKE '%index%' GROUP BY date1,cs_uri_stem order by month1, year1) p) group by p.month1,p.year1 order by p.year1,p.month1;
